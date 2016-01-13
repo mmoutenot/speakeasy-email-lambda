8b9cc8bdce5b6ee76fca17134b6f144d35a191de
mmoutenot <mmoutenot@gmail.com>
2016-01-12 23:41:13 -0500

    init

---
+++ config.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+var config = {
+  "templateBucket": "speakeasy-email-templates",
+  "templateKey": "test.html",
+  "targetAddress": "mmoutenot@gmail.com",
+  "fromAddress": "Speakeasy <mmoutenot@gmail.com>"
+}
+
+module.exports = config
